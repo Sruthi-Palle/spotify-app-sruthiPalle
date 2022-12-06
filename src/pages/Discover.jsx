1b23203c74f59898c23261cbc2660d4a0129b082
Sruthi-Palle <pallesruthi12@gmail.com>
2022-12-06 15:50:21 +0530

    modified Discover.jsx and created drop down with genre options

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,31 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { useState } from "react";
+
+const Discover = () => {
+  const [genreTitle, setGenreTitle] = useState(genres[0].title);
+
+  console.log(genres);
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h1 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h1>
+        <select
+          onChange={(e) => setGenreTitle(e.target.value)}
+          value=""
+          className="bg-black text-gray-300 text-sm p-3 rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.title}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
