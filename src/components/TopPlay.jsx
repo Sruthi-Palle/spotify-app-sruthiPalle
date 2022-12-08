25bdb3eaf3c76d806e865993d9eebc61c71f27d4
Sruthi-Palle <pallesruthi12@gmail.com>
2022-12-09 02:07:04 +0530

    created responsive Sidebar component and started working on TopPlay component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,34 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import "swiper/css";
+import "swiper/css/free-mode";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behaviour: "smooth" });
+  });
+
+  const topPlays = data?.slice(0, 5);
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return <div ref={divRef} className=""></div>;
+};
 
 export default TopPlay;
