dbda934567cba9c538a8aca3c8a375abea58744e
Sruthi-Palle <pallesruthi12@gmail.com>
2022-12-09 14:50:21 +0530

    created TopPlay parent component which has TopChartCard component as child component and reused playPause component in TopChartsCard component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -8,6 +8,44 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 import "swiper/css";
 import "swiper/css/free-mode";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const TopChartCard = ({
+  song,
+  i,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => {
+  return (
+    <div className="w-full flex flex-row items-center hover:bg-[4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+      <h3 className="text-white text-base font-bold mr-3">{i + 1}.</h3>
+      <div className="flex-1 flex flex-row justify-between items-center">
+        <img
+          src={song?.images?.coverart}
+          alt={song?.title}
+          className="rounded-lg h-20 w-20"
+        />
+        <div className="flex-1 flex flex-col justify-center mx-3">
+          <Link to={`/songs/${song.key}`}>
+            <p className="text-xl font-bold text-white">{song?.title} </p>
+          </Link>
+          <Link to={`/artists/${song?.artists[0].adamid}`}>
+            <p className="text-base  text-gray-300 mt-1">{song?.subtitle} </p>
+          </Link>
+        </div>
+      </div>
+      <PlayPause
+        song={song}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePause={handlePauseClick}
+        handlePlay={handlePlayClick}
+      />
+    </div>
+  );
+};
+
 const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
@@ -23,12 +61,72 @@ const TopPlay = () => {
     dispatch(playPause(false));
   };
 
-  const handlePlayClick = () => {
-    dispatch(setActiveSong({ song, data, i }));
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, i, data }));
     dispatch(playPause(true));
   };
 
-  return <div ref={divRef} className=""></div>;
+  return (
+    <div
+      ref={divRef}
+      className="flex-1 flex flex-col xl:max-w-[500px] max-w-full  xl:ml-6 ml-0 xl:mb-0 mb-6 "
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard
+              song={song}
+              i={i}
+              key={song.key}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4 "
+        >
+          {topPlays?.map((song, i) => (
+            <SwiperSlide
+              key={song?.key}
+              style={{ width: "25%", height: "auto" }}
+              className="shadow-lg rounded-full animate-slideright"
+            >
+              <Link to={`/artists/${song?.artists[0].adamid}`}>
+                <img
+                  src={song?.images.background}
+                  alt="name"
+                  className="rounded-full w-full object-cover"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default TopPlay;
