b1f2ac10b8eef0b1a0eb48beafe0329d3a5db8c3
Sruthi-Palle <pallesruthi12@gmail.com>
2022-12-09 15:28:38 +0530

    added hover styles to the TopChartCard component song title and subtitle

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -28,10 +28,14 @@ const TopChartCard = ({
         />
         <div className="flex-1 flex flex-col justify-center mx-3">
           <Link to={`/songs/${song.key}`}>
-            <p className="text-xl font-bold text-white">{song?.title} </p>
+            <p className="text-xl font-bold text-white hover:bg-[#342e4e] rounded-lg pl-4 truncate">
+              {song?.title}{" "}
+            </p>
           </Link>
           <Link to={`/artists/${song?.artists[0].adamid}`}>
-            <p className="text-base  text-gray-300 mt-1">{song?.subtitle} </p>
+            <p className="text-base  text-gray-300 mt-1 hover:bg-[#342e4e] rounded-lg pl-4 truncate">
+              {song?.subtitle}{" "}
+            </p>
           </Link>
         </div>
       </div>
