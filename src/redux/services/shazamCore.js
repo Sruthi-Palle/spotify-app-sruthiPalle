39d8f26396aa1e9d186ed4ab0cae1fe46e4637d3
Sruthi-Palle <pallesruthi12@gmail.com>
2022-12-12 19:36:57 +0530

    created songDetails component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -32,6 +32,9 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongDetails: builder.query({
+      query: (songid) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
