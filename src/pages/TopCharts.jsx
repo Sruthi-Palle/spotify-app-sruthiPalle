9edfb5bff3799e54d311da6d2d1d99f31ea98001
Sruthi-Palle <pallesruthi12@gmail.com>
2022-12-15 15:23:14 +0530

    added search functionality

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,30 +1,16 @@
 import React from "react";
-import { useState, useEffect } from "react";
+
 import { useSelector } from "react-redux";
-import axios from "axios";
 
 import { Error, Loader, SongCard } from "../components";
-import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const TopCharts = () => {
-  const [country, setCountry] = useState("");
-  const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+  const { data, isFetching, error } = useGetTopChartsQuery();
 
-  console.log(country);
-  useEffect(() => {
-    axios
-      .get(
-        `https://geo.ipify.org/api/v2/country?apiKey=at_esjar6PFYlbulBZMtdkkzgAwrpJPP`
-      )
-      .then((res) => setCountry(res?.data?.location?.country))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
-  }, [country]);
-
-  if (isFetching && loading) {
-    return <Loader title="Loading songs around you" />;
+  if (isFetching) {
+    return <Loader title="Loading Top Charts" />;
   }
   if (error && country) {
     return <Error />;
@@ -33,7 +19,7 @@ const TopCharts = () => {
   return (
     <div className="flex flex-col ">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-        Around You <span className="font-black">{country}</span>
+        Discover Top Charts
       </h2>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
